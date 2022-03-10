6bc1ac76c5cb4e81a775134a14b7acda75ce9bbe
Buff4eyes <claudemokbel@gmail.com>
2022-03-10 13:50:54 -0800

    added some more packages

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose')
+
+const User = new Schema({
+  username: String,
+  email: String,
+  categories: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'category'
+    }],
+  month: {
+    type: Schema.Types.ObjectId,
+    ref: 'month'
+  }
+}, { timestamps: true })
+User.plugin(require('passport-local-mongoose'));
+
+module.exports = model('User', User) 
