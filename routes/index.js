c2f89aaf41f14092f4be6cc7a2af7aa2b4cf12ba
Buff4eyes <claudemokbel@gmail.com>
2022-03-10 14:11:04 -0800

    route boiler plate

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+
+router.use('/api', require('./userRoutes.js'))
+
+router.use('/api', require('./blogRoutes.js'))
+
+router.use('/api', require('./noteRoutes.js'))
+
+module.exports = router
