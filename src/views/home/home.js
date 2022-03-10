983e3f4bfd5e7c72a44fd602c407eb5846e6e76f
Buff4eyes <claudemokbel@gmail.com>
2022-03-10 14:56:47 -0800

    creating the home page

---
+++ src/views/home/home.js
@@ -0,0 +1,18 @@
+const Home = (props) => {
+  return (
+    <>
+        <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand mx-auto" href="/">
+          <img src="./Stachecurlssticker.png" alt="logo" width="75" height="75"/>
+          Welcome to Claude's Corner
+          <img src="./Stachecurlssticker.png" alt="logo" width="75" height="75"/>
+        </a>
+    </nav>
+    <div className="blog-container">
+      testing 1 2 3
+    </div>
+    </>
+  )
+}
+
+module.exports = Home
