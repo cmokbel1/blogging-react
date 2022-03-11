e0e6ecd5444a462769470dde7b1d7e1a89b117f5
Buff4eyes <claudemokbel@gmail.com>
2022-03-10 18:47:02 -0800

    login component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Footer } from './footer'
 export { default as BlogContainer } from './blogContainer'
+export { default as Login } from './login'
---
+++ src/components/login.js
@@ -0,0 +1,20 @@
+const Login = (props) => {
+  return (
+    <>
+      <div className="mb-3 row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <div className="col-sm-10">
+      <input type="text" readonly className="form-control-plaintext" id="inputEmail" value="email@example.com" />
+    </div>
+  </div>
+  <div className="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+    <div className="col-sm-10">
+      <input type="password" className="form-control" id="inputPassword" />
+    </div>
+  </div>
+  </>
+  )
+}
+
+export default Login
