ee48a28d4178cef6c1e04a6e9e59870f21fca2aa
Buff4eyes <claudemokbel@gmail.com>
2022-03-26 15:21:31 -0700

    beep boop

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,19 +1,28 @@
 import logo from './Stachecurlssticker.png';
 import { LogOrReg } from '../LoginRegister/logOrReg';
 import { Poster } from '../../views/Poster/index';
+import { Route, Routes, BrowserRouter as Router, Link } from 'react-router-dom'
+
 export function NavBar() {
 
   if (localStorage.getItem('token') && localStorage.getItem('username') === 'Claude') {
     return (
-      <nav className="navbar navbar-dark bg-dark">
-        <button>Blog</button>
-        <a className="navbar-brand mx-auto" href="/">
-          <img src={logo} alt="logo" width="75" height="75" />
-          Welcome to Claude's Corner
-          <img src={logo} alt="logo" width="75" height="75" />
-        </a>
-        <LogOrReg />
-      </nav>
+      <>
+        <nav className="navbar navbar-dark bg-dark">
+          <Router>
+            <Routes>
+              <Route path="/post" element={<Poster />}></Route>
+            </Routes>
+            <Link to="/post">Blog</Link>
+          </Router>
+          <a className="navbar-brand mx-auto" href="/">
+            <img src={logo} alt="logo" width="75" height="75" />
+            Welcome to Claude's Corner
+            <img src={logo} alt="logo" width="75" height="75" />
+          </a>
+          <LogOrReg />
+        </nav>
+      </>
     )
   } else {
     return (
