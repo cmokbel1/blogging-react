29acb8d1c610a95d37887f80113f88696f7e772c
Buff4eyes <claudemokbel@gmail.com>
2022-03-26 15:49:10 -0700

    working on router fix

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -4,17 +4,16 @@ import { Poster } from '../../views/Poster/index';
 import { Route, Routes, BrowserRouter as Router, Link } from 'react-router-dom'
 
 export function NavBar() {
-
+  <Router>
+    <Routes>
+      <Route path="/post" element={<Poster />}></Route>
+    </Routes>
+    <Link to="/post">Blog</Link>
+  </Router>
   if (localStorage.getItem('token') && localStorage.getItem('username') === 'Claude') {
     return (
       <>
         <nav className="navbar navbar-dark bg-dark">
-          <Router>
-            <Routes>
-              <Route path="/post" element={<Poster />}></Route>
-            </Routes>
-            <Link to="/post">Blog</Link>
-          </Router>
           <a className="navbar-brand mx-auto" href="/">
             <img src={logo} alt="logo" width="75" height="75" />
             Welcome to Claude's Corner
