fffe47390bf08f3b5af6ab74057f0522974d85fa
Buff4eyes <claudemokbel@gmail.com>
2022-03-27 18:32:46 -0700

    trying to render comment modal

--- src/components/Blog/blog.js
+++ src/components/Blog/blog.js
@@ -1,28 +1,46 @@
+import { useState } from 'react'
 import './blog.css';
+import {HandleNoteModal} from '../NoteModal/notemodal'
 
 const Blog = (props) => {
   const { blog } = props
+
+  
+  const [showModal, setShowModal] = useState(null)
+
+   function handleModalSwitch()  {
+    setShowModal(true)
+    console.log(showModal)
+   }
+
+  if (showModal) {
+    return (
+      <HandleNoteModal />
+    )
+  }
+
   return (
     <>
-    <div className="blogBox">
-      <h4>{blog.title}</h4>
-      <hr />
-      <p>{blog.body}</p>
-      <hr />
-      <p id="postedBy">Posted By: Claude @ {blog.createdAt}</p>
-      <hr />
-      <button className="btn btn-info">Comment</button>
-      <hr />
-      <div className="comment Section">
-        <span>User 1:</span>
-        <span> Comment one</span>
-        <br />
-        <span>User 2:</span>
-        <span> Comment two</span>
+      <div className="blogBox">
+        <h4>{blog.title}</h4>
+        <hr />
+        <p>{blog.body}</p>
+        <hr />
+        <p id="postedBy">Posted By: Claude @ {blog.createdAt}</p>
+        <hr />
+        <button className="btn btn-info" onClick={handleModalSwitch}>Comment</button>
+        <hr />
+        <div className="comment Section">
+          <span>User 1:</span>
+          <span> Comment one</span>
+          <br />
+          <span>User 2:</span>
+          <span> Comment two</span>
+        </div>
       </div>
-    </div>
     </>
   )
+
 }
 
 
