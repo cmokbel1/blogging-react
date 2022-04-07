9c63fd5aee987017812109b91876fb96e307f195
Buff4eyes <claudemokbel@gmail.com>
2022-04-07 13:07:52 -0700

    removed the notemodal

--- src/components/Blog/blog.js
+++ src/components/Blog/blog.js
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import './blog.css';
-import { HandleNoteModal } from '../NoteModal/notemodal'
 
 const Blog = (props) => {
   const { blog, idx, id } = props
 
+  const [noteState, setNoteState] = useState({
+    user: '',
+    note: ''
+  });
 
-  const [open, setOpen] = useState({});
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const handleInputChange = ({ target: { name, value } }) => {
+    setNoteState({ ...noteState, [name]: value });
+  };
 
 
   return (
@@ -20,15 +23,12 @@ const Blog = (props) => {
         <hr />
         <p id="postedBy">Posted By: Claude @ {blog.createdAt}</p>
         <hr />
-        <button className="btn btn-info" onClick={() => handleOpen}>Comment</button>
-        <HandleNoteModal open={open} onClose={() => handleClose} />
+        <label htmlFor="comment" className="col-sm-2 col-form-label">Leave a Comment:</label>
+          <textarea type="text" className="form-control"  name="comment" onChange={handleInputChange}  placeholder=" " />
+          <br />
+        <button className="btn btn-info" >Comment</button>
         <hr />
         <div className="comment Section">
-          <span>User 1:</span>
-          <span> Comment one</span>
-          <br />
-          <span>User 2:</span>
-          <span> Comment two</span>
         </div>
       </div>
     </>
